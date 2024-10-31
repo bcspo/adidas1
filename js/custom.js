a11e4f37ea04eee68b75a8806bdf7882dffdfe5e
bcspo <169110098+bcspo@users.noreply.github.com>
2024-10-31 17:04:52 +0900

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,58 @@
+$(function () {
+    var swiper = new Swiper(".slide_point", {
+        slidesPerView: 3.5,
+        spaceBetween: 30,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+
+    var swiper = new Swiper(".slide_shoes", {
+        slidesPerView: 5,
+        scrollbar: {
+            el: ".swiper-scrollbar",
+            // hide: true,
+        },
+
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+
+    var swiper = new Swiper(".slide_clothing", {
+        slidesPerView: 5,
+        scrollbar: {
+            el: ".swiper-scrollbar",
+            // hide: true,
+        },
+
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+
+    var swiper = new Swiper(".slide_accessory", {
+        slidesPerView: 5,
+        scrollbar: {
+            el: ".swiper-scrollbar",
+            // hide: true,
+        },
+
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+
+    var swiper = new Swiper(".slide_best", {
+        slidesPerView: 3.5,
+        spaceBetween: 30,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+})
